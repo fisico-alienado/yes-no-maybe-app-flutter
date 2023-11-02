f905f6ca05fab02da3e18aa819974c452105b13d
Rubén del Mazo Rodríguez <ruben.delmazorodriguez@gmail.com>
2023-11-02 12:06:02 +0100

    Instalacion provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:yes_no_maybe_app/config/theme/app_theme.dart';
+import 'package:yes_no_maybe_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_maybe_app/presentation/screens/chat/chat_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -10,22 +12,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Yes No Maybe App',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 1).theme(),
-      home: const ChatScreen()
-      // home: Scaffold(
-      //   appBar: AppBar(
-      //     title: const Text('Yes No Maybe App Bar'),
-      //   ),
-      //   body: Center(
-      //     child: FilledButton.tonal(
-      //       onPressed: (){ }, 
-      //       child: const Text('Click me')
-      //     ),
-      //   ),
-      // ),
+    return MultiProvider( 
+      // en este caso, el gestor de estado PROVIDER tiene que estar al tanto de todos los hijos de la app y por eso lo ponemos lo mas alto posible en el organigrama de jerarquia
+      // Usamos 'MultiProvider' porque puede que tengamos de un proveedor/clase de informacion y este widget permite añadir varios (por ejemplo, autentificacion, gestor de temas, chat, etc)
+      providers: [
+        ChangeNotifierProvider(create: (_) => ChatProvider()) // si no se necesita el argumento, pero es obligatorio ponerlo, se pone "_"
+        // * AL HABER ENVUELTO TODA LA APP CON ESTE PROVEEDOR, TODOS LO WIDGETS HIJOS DE MyApp() tienen acceso al provider ChatProvider()
+      ],
+      child: MaterialApp(
+        title: 'Yes No Maybe App',
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme(selectedColor: 1).theme(),
+        home: const ChatScreen()
+        // home: Scaffold(
+        //   appBar: AppBar(
+        //     title: const Text('Yes No Maybe App Bar'),
+        //   ),
+        //   body: Center(
+        //     child: FilledButton.tonal(
+        //       onPressed: (){ }, 
+        //       child: const Text('Click me')
+        //     ),
+        //   ),
+        // ),
+      ),
     );
   }
 }
