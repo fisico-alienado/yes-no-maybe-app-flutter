3d0890ec4c41af2b66aba6546eeb9945e5f9f9de
Rubén del Mazo Rodríguez <ruben.delmazorodriguez@gmail.com>
2023-11-03 15:07:23 +0100

    Fin de la app

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -8,8 +8,8 @@ class ChatProvider extends ChangeNotifier{
   final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messageList = [
-    Message(text: 'Hola amor!', fromWho: FromWho.me),
-    Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me)
+    // Message(text: 'Hola amor!', fromWho: FromWho.me),
+    // Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me)
   ];
 
   Future<void> sendMessage(String text) async {
